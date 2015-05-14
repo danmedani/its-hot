bee138292fab26aa5ef008acf46767085a7ad93d
danmedani <danmedani@gmail.com>
2015-05-13 17:10:29 -0700

    updates

--- app/app.js
+++ app/app.js
@@ -1,69 +1,87 @@
-var express = require('express');
-var app = express();
+var express = require('express')
+var app = express()
+var moment = require('moment')
+
+app.set('view engine', 'ejs')
 
 var bodyParser = require('body-parser')
-app.use(bodyParser());
+app.use(bodyParser())
 
-var MongoClient = require('mongodb').MongoClient;
+var MongoClient = require('mongodb').MongoClient
 
 var dbConnect = function(then) { 
-  MongoClient.connect('mongodb://localhost:27017/posts', then); 
+  MongoClient.connect('mongodb://localhost:27017/posts', then) 
+}
+
+var getRenderedPosts = function(then) {
+  dbConnect(function (err, db) {
+    var posts = db.collection('post')
+    var retPosts = []
+    posts.find().sort({ moment: -1 }).toArray(function(err, items) {
+      var numToShow = 6
+      if (items.length < 6) {
+        numToShow = items.length
+      }
+      for (var i = 0; i < numToShow; i ++) {
+        items[i].style = 'font-size:' + (400 - (i * 70)) + '%;color:' + randomHexColor()
+        retPosts.push(items[i])
+      }
+      then(retPosts)
+    })
+  })
+}
+
+var randomHexColor = function() {
+  return '#'+Math.floor(Math.random()*16777215).toString(16);
 }
 
 app.get('/', function (req, res) {
-  dbConnect(function (err, db) {
-    var posts = db.collection('post');
-    posts.find().toArray(function(err, items) {
-      res.send(items);
-
-    });
-  });
-});
+  getRenderedPosts(function(renderedItems) {
+    res.render('layout', { posts: renderedItems, bgColor: "#66F0A8" })
+  })
+})
 
 var insertPost = function (db, inPost, then) {
   db.createCollection('post', function (err, collection) {
-    var testPost = {'user': inPost.user, 'post': inPost.post};
+    var testPost = {'user': inPost.user, 'post': inPost.post, 'moment': moment()}
     collection.insert(testPost, {w:1}, function(err, result) {
       if (err) {
-        then('database insertion error');
+        then('database insertion error')
       }
 
       if (result.result.ok && result.result.n == 1) {
-        then(null, '<html><body>ayoooo</body></html>');
+        then(null)
       } else {
-        then('db insert did not work');  
+        then('db insert did not work')  
       }
-    });
-  });
-};
+    })
+  })
+}
 
 app.post('/', function (req, res) {
-  var inPost = req.body;
-  if (inPost && inPost.user && inPost.post) {
+  var inPost = req.body
+  if (inPost && inPost.user) {
     dbConnect(function(err, db) {
-      if (err) {
-        res.send('database connection errror');
-      }
+      if (err) res.send('database connection errror')
 
-      insertPost(db, inPost, function(err, result) {
-        if (err) {
-          res.send(err);
-        }
+      insertPost(db, inPost, function(err) {
+        if (err) res.send(err)
         
-        res.send(result);
+        getRenderedPosts(function(renderedItems) {
+          res.render('layout', { posts: renderedItems, bgColor: "#66F0A8" })
+        })
       })
-    });
+    })
   } else {
-    res.send('invalid request');
+    res.send('invalid request')
   }
-});
+})
 
 var server = app.listen(3000, function () {
- var host = server.address().address;
- var port = server.address().port;
+ var host = server.address().address
+ var port = server.address().port
 
- console.log('Damn... It is HOT');
+ console.log('Damn... It is HOT')
 
-});
-
+})
 
